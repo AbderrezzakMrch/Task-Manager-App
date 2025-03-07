4ba7e7396527b7ad6cbae18703ffaa0749710ae8
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-03-07 10:05:32 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Dashboard from './components/Dashboard';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
@@ -9,7 +8,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Login />} /> {/* Default route */}
       </Routes>
     </Router>
   );
