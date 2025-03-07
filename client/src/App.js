8bb505e538de75395cdf9f8d65d3b6fb8adec0b0
Abdou.exe <147605515+AbderrezzakMrch@users.noreply.github.com>
2025-03-07 10:10:06 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Login from "./components/Login";
+import Signup from "./components/Signup";
 import Dashboard from "./components/Dashboard";
 
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
